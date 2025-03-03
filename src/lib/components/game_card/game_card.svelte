0b3bf12156b6de1c9afa6bca5768a5088f182cd7
haruki-nikaidou <nharuki@tuta.com>
2025-03-04 03:30:45 +0900

    save

--- src/lib/components/game_card/game_card.svelte
+++ src/lib/components/game_card/game_card.svelte
@@ -7,17 +7,24 @@
         title: string;
         size: number;
         uuid: string;
+        updated_at: string;
+        created_at: string;
+        download_provider: string;
     }
 
-    let { title, uuid, size }: Props = $props();
+    let { title, uuid, size, updated_at, created_at, download_provider }: Props = $props();
 
 </script>
 
 <Card.Root>
     <Card.Header>
         <Card.Title>{title}</Card.Title>
-        <Card.Description>大小： {filesize(size)}</Card.Description>
     </Card.Header>
+    <Card.Content class="text-sm text-muted-foreground">
+        <p>大小：{filesize(size)}</p>
+        <p>更新时间：{updated_at.toLocaleString()}</p>
+        <p>创建时间：{created_at.toLocaleString()}</p>
+    </Card.Content>
     <Card.Footer>
         <Button>下载</Button>
     </Card.Footer>
